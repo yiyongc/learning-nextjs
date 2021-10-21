7079ddaf9855c2ed7e802a365f4141bb3bd47229
Yi Yong <yiyongc@users.noreply.github.com>
2021-10-21 22:27:50 +0800

    Added featured posts component and rendered it using dummy data, also used posts in all posts page. Started on Post Details page and rendered markdown using react-markdown

---
+++ nextjs-blog-app/components/home-page/featured-posts.js
@@ -0,0 +1,13 @@
+import PostsGrid from "../posts/posts-grid";
+import styles from "./featured-posts.module.css";
+
+function FeaturedPosts(props) {
+  return (
+    <section className={styles.latest}>
+      <h2>Featured Posts</h2>
+      <PostsGrid posts={props.posts} />
+    </section>
+  );
+}
+
+export default FeaturedPosts;
