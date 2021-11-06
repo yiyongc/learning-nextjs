2137fdea94b0f7e10fe66621a628aa73b483b9ae
Yi Yong <yiyongc@users.noreply.github.com>
2021-11-06 23:44:32 +0800

    Added contact form with API route integration to send contact form data to API

---
+++ nextjs-blog-app/components/contact/contact-form.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import styles from "./contact-form.module.css";
+
+function ContactForm() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    // optionally add client side validation
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  return (
+    <section className={styles.contact}>
+      <h1>How can I help you?</h1>
+      <form className={styles.form} onSubmit={sendMessageHandler}>
+        <div className={styles.controls}>
+          <div className={styles.control}>
+            <label htmlFor="email">Your Email</label>
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(event) => setEnteredEmail(event.target.value)}
+            />
+          </div>
+          <div className={styles.control}>
+            <label htmlFor="name">Your Name</label>
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(event) => setEnteredName(event.target.value)}
+            />
+          </div>
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="message">Your Message</label>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            value={enteredMessage}
+            onChange={(event) => setEnteredMessage(event.target.value)}
+          ></textarea>
+        </div>
+        <div className={styles.actions}>
+          <button>Send Message</button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
